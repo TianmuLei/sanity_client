79439351cd321d0747da109e6faa17b73667ab2a
Ruyin Shao <ruyinsha@usc.edu>
2017-10-16 13:04:02 -0700

    add transaction picker done

--- Sanity/AddTransactionPage.m
+++ Sanity/AddTransactionPage.m
@@ -8,21 +8,38 @@
 
 #import "AddTransactionPage.h"
 
-@interface AddTransactionPage ()
+@interface AddTransactionPage () <UIPickerViewDelegate, UIPickerViewDataSource>
 @property (weak, nonatomic) IBOutlet UITextField *budgetTF;
 @property (weak, nonatomic) IBOutlet UIDatePicker *date;
 @property (weak, nonatomic) IBOutlet UITextField *amountTF;
 @property (weak, nonatomic) IBOutlet UITextField *categoryTF;
 @property (weak, nonatomic) IBOutlet UITextField *descripTF;
-
+@property UIPickerView *budgetPicker;
+@property UIPickerView *categoryPicker;
+@property (strong, nonatomic) NSMutableArray *budgets;
 @end
 
 @implementation AddTransactionPage
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    UIPickerView *yourpicker = [[UIPickerView alloc] initWithFrame:CGRectMake(0, 50, 100, 150)];
-//    [yourpicker setDataSource: self];
+    _budgets = [[NSMutableArray alloc]init];
+    [_budgets addObject:@"budget 1"];
+    [_budgets addObject:@"budget 2"];
+    
+    self.budgetPicker = [[UIPickerView alloc] initWithFrame:CGRectMake(0, 50, 100, 150)];
+    self.budgetPicker.delegate = self;
+    self.budgetPicker.dataSource = self;
+    self.budgetPicker.showsSelectionIndicator = YES;
+    _budgetTF.inputView = self.budgetPicker;
+    
+    self.categoryPicker = [[UIPickerView alloc] initWithFrame:CGRectMake(0, 50, 100, 150)];
+    self.categoryPicker.delegate = self;
+    self.categoryPicker.dataSource = self;
+    self.categoryPicker.showsSelectionIndicator = YES;
+    _categoryTF.inputView = self.categoryPicker;
+    
+ 
 //    [yourpicker setDelegate: self];
 //    yourpicker.showsSelectionIndicator = YES;
 //    self.budgetTF.inputView = yourpicker;
@@ -38,7 +55,69 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - UIPickerViewDataSource
+// #3
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    if (pickerView == self.budgetPicker|| pickerView == self.categoryPicker) {
+        return 1;
+    }
+
+    
+    return 0;
+}
+
+// #4
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    if (pickerView == self.budgetPicker) {
+        return [self.budgets count];
+    }
+    if (pickerView == self.categoryPicker) {
+#warning  tobe changed
+        return [self.budgets count];
+    }
+    
+    return 0;
+}
+
+#pragma mark - UIPickerViewDelegate
+
+// #5
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    if (pickerView == self.budgetPicker) {
+        return self.budgets[row];
+    }
+    if (pickerView == self.categoryPicker) {
+        return self.budgets[row];
+    }
+    
+    return nil;
+}
+
+// #6
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    if (pickerView == self.budgetPicker) {
+        self.budgetTF.text = self.budgets[row];
+    }
+    if (pickerView == self.categoryPicker) {
+        self.categoryTF.text = self.budgets[row];
+    }
+}
+
+
 #pragma mark - Table view data source
+- (IBAction)submitTransaction:(id)sender {
+    if (_amountTF.text.length < 1) {
+        [self getAlerted:@"Required Fields" msg:@"Please fill in all required fields"];
+    }
+    else if (![self numberFormatChecker:_amountTF.text]){
+         [self getAlerted:@"Number format Error" msg:@"Please enter numbers for budget amount"];
+    
+    }
+    else {
+        //get elements
+    }
+
+}
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 2;
@@ -48,6 +127,33 @@
     return [super tableView:tableView numberOfRowsInSection:section];
 
 }
+
+//alert window
+- (void) getAlerted: (NSString*) errorTitle msg:(NSString*) errorMessage {
+    UIAlertController *alertController = [UIAlertController
+                                          alertControllerWithTitle:errorTitle
+                                          message:errorMessage
+                                          preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okAction = [UIAlertAction
+                               actionWithTitle:@"OK"
+                               style:UIAlertActionStyleDefault
+                               handler:^(UIAlertAction *action){
+                                   //set all label to red
+                               }];
+    [alertController addAction:okAction];
+    [self presentViewController:alertController animated:YES completion:nil];
+    
+}
+
+//return NO if not numeric
+- (BOOL) numberFormatChecker: (NSString *) mString{
+    NSNumberFormatter *nf = [[NSNumberFormatter alloc] init];
+    BOOL isDecimal = [nf numberFromString:mString] != nil;
+    
+    return isDecimal;
+}
+
+
 
 /*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
