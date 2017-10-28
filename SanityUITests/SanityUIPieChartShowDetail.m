5a20537f8a7ea80f4001f9bd86ee0878e1b12821
Gu <ziqiangu@usc.edu>
2017-10-28 01:40:12 -0700

    Pie Chart UI Tests added

---
+++ SanityUITests/SanityUIPieChartShowDetail.m
@@ -0,0 +1,56 @@
+//
+//  SanityUIPieChartShowDetail.m
+//  Sanity
+//
+//  Created by Gu on 10/28/17.
+//  Copyright © 2017 Absolute A. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SanityUIPieChartShowDetail : XCTestCase
+
+@end
+
+@implementation SanityUIPieChartShowDetail
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testPieChartShowDetail {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch];
+    //login
+    XCUIElement *emailTextField = app.textFields[@"Email"];
+    [emailTextField tap];
+    [emailTextField typeText:@"z@z.com"];
+    XCUIElement *passwordSecureTextField = app.secureTextFields[@"Password"];
+    [passwordSecureTextField tap];
+    [passwordSecureTextField typeText:@"zg"];
+    [app.buttons[@"Done"] tap];
+    [app.buttons[@"Login In"] tap];
+    
+    XCUIElementQuery *tablesQuery = app.tables;
+    [tablesQuery.cells[@"budget1, 0/500"] tap];
+    //tap slice0 in pie chart
+    [[[[[[[[[[[[app childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element tap];
+    //check if label exists
+    XCTAssert(app.staticTexts[@"Cat1,   0.00/500.00"].exists);    
+}
+
+@end
